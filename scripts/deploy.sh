4d0936a072ad14ecba5d378ed783adb86cb57721
greg <gregory.diamos@gmail.com>
2022-09-20 08:42:18 -0700

    deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Safely execute this bash script
+# e exit on first failure
+# x all executed commands are printed to the terminal
+# u unset variables are errors
+# a export all variables to the environment
+# E any trap on ERR is inherited by shell functions
+# -o pipefail | produces a failure code if any stage fails
+set -Eeuoxa pipefail
+
+git subtree push --prefix dist origin gh-pages
+
